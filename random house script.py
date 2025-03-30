10406bcde9ffe916161d5a9262b4093a1e6b5c73
MeVietName <thibautnemo@gmail.com>
2025-03-30 23:22:31 +0700

    correction

--- random house script.py
+++ random house script.py
@@ -1,11 +1,11 @@
 import random
 
-# Islands and Cities with max house numbers
+# Islands + Cities with max house numbers
 Ironbard = {"Vardo": 724, "Hofn": 1038, "Helvig": 557, "Bjurholm": 467, "Blonduos": 456, "Helluland": 440}
 Providence = {"Hayarano": 438, "Akkeshi": 469, "Reading": 857, "Nelson": 470, "Arcadia": 1693, "Kiyobico": 616, "Takazaki": 581, "Biruwa": 547, "Shinobi": 529}
 BonneSante = {"Nidoma": 492, "Talu": 398, "Pauma": 455, "Riroua": 538, "Valais": 526, "Maeva": 585, "Gordes": 507, "Kinsale": 353, "Colmar": 2075, "Vaiku": 478, "Mahuti": 1064, "Eden": 614}
 
-# Island
+# Islands
 islands = {"Ironbard": Ironbard, "Providence": Providence, "BonneSante": BonneSante}
 
 def randomfunc():
@@ -21,8 +21,8 @@ def generate_random_houses(n=60, filename="random_houses.txt"):
     with open(filename, "w") as file:
         for island, city, house in houses:
             line = f"Island: {island}, City: {city}, House Number: {house}\n"
-            print(line.strip())  # Print to console
-            file.write(line)  # Write to file
+            print(line.strip())
+            file.write(line) 
 
-# Generate 60 random houses
+
 generate_random_houses()
